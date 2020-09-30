e6988d367e85df0dd3d399c808dff177549405b9
Bao Nguyen <binbonbonben@gmail.com>
2020-09-29 22:17:50 -0500

    Add Alert modal

---
+++ client/src/components/CommonView/AlertPanel/AlertPanel.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {Modal, Button} from "react-bootstrap";
+
+function AlertPanel(props) {
+    console.log(props.alert);
+
+    return (
+        <Modal show={props.alert} onHide={() => props.onClose()}>
+        <Modal.Header closeButton>
+          <Modal.Title>{props.heading}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{props.content}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="dark" onClick={() => props.onClose()}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+
+}
+
+export default AlertPanel;
--- client/src/components/HomeView/SliderImages/SliderImages.jsx
+++ client/src/components/HomeView/SliderImages/SliderImages.jsx
@@ -1,33 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Carousel, Row, Col, Button } from 'react-bootstrap';
 //import bgImages from '../../../resouces/images/backgroundImages/bgImages';
 import './style.css';
-//import img1 from '../../../resouces/images/backgroundImages/bg_1.jpg'
+//import img1 from '../../../resouces/images/backgroundImages/bg_1.jpg';
+import AlertPanel from '../../CommonView/AlertPanel/AlertPanel';
 
+// This component to show the slider image on homepage
 function SliderImages(props) {
+  // list of images
   const images = props.images;
-  return (
-    <Carousel>
-      {images.map((item) => {
-        return (
-          <Carousel.Item key={item.alt}>
-            <img className="d-block w-100" src={process.env.REACT_APP_PHOTO_URL + item.src} alt={item.alt} />
-            <Carousel.Caption className="caption-pos">
-              <span className="subheading">Welcome</span>
-              <Row>
-                <Col lg={{ span: 6, offset: 3 }}>
-                  <h1>{item.title}</h1>
-                </Col>
-              </Row>
 
-              <p>{item.content}</p>
-              <Button variant="light">Order Now</Button>
-              <Button variant="light">View Menu</Button>
-            </Carousel.Caption>
-          </Carousel.Item>
-        );
-      })}
-    </Carousel>
+  // To control alert panel when clich on Order Now button
+  const [alert, setAlert] = useState(false);
+
+  function hideAlert() {
+    setAlert(false);
+  }
+
+  function showAlert() {
+    console.log("ordering");  
+    setAlert(true);
+  }
+
+  return (
+    <React.Fragment>
+      <AlertPanel onClose={hideAlert} heading="Alert Heading" content="Alert Content" alert={alert}></AlertPanel>
+      <Carousel>
+        {images.map((item) => {
+          return (
+            <Carousel.Item key={item.alt}>
+              <img className="d-block w-100" src={process.env.REACT_APP_PHOTO_URL + item.src} alt={item.alt} />
+              <Carousel.Caption className="caption-pos">
+                <span className="subheading">Welcome</span>
+                <Row>
+                  <Col lg={{ span: 6, offset: 3 }}>
+                    <h1>{item.title}</h1>
+                  </Col>
+                </Row>
+
+                <p>{item.content}</p>
+                <Button variant="light" onClick={showAlert}>Order Now</Button>
+                <Button variant="light">View Menu</Button>
+              </Carousel.Caption>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+    </React.Fragment>
+
   );
 }
 
