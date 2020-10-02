25bdfec37d677a7c62dfad7968b6553ce56ad8f9
Thien Nguyen <thien.nguyen30191@gmail.com>
2020-10-01 22:53:19 -0500

    add route to products able to complete GET POST GETID DELETE need to fix products SCHEMA for drink and dessert

--- server/controllers/info-controllers.js
+++ server/controllers/info-controllers.js
@@ -7,59 +7,66 @@ const Info = require('../models/info');
 const HttpError = require('../models/http-error');
 
 // Get the info
-const getInfo =  async(req, res, next) => {
-    console.log('get info');
+const getInfo = async (req, res, next) => {
+  console.log('get info');
 
-    let info;
-    try {
-        info = await Info.findOne();
-        //console.log(info);
-    } catch(err){
-        const error = new HttpError('error when getting info', 500);
-        return next(error);
-    }
+  let info;
+  try {
+    info = await Info.findOne();
+    //console.log(info);
+  } catch (err) {
+    const error = new HttpError('error when getting info', 500);
+    return next(error);
+  }
 
-    if(!info){
-        const error = new HttpError('info is undefined', 404);
-        return next(error);
-    }
+  if (!info) {
+    const error = new HttpError('info is undefined', 404);
+    return next(error);
+  }
 
-    res.json({info: info.toObject({getters: true})}); // getters: true => create an id for object
+  res.json({ info: info.toObject({ getters: true }) }); // getters: true => create an id for object
 };
-
 
 // update the info
 const updateInfo = (req, res, next) => {
-    console.log('update info');
+  console.log('update info');
 };
 
 // add new info
 const addInfo = async (req, res, next) => {
-    const {name,shortAddress, address, phoneNumber, images, intro, fullIntro, bestDrinksIntro, hours, hoursDetail} = req.body;
-    const newInfo = new Info({
-        name,
-        shortAddress, 
-        address,
-        phoneNumber,
-        images,
-        intro,
-        fullIntro,
-        bestDrinksIntro,
-        hours,
-        hoursDetail
-    });
-    
-    try {
-        await newInfo.save();
-    } catch (err){
-        const error = new HttpError('error when adding new info', 500);
-        return next(error);
-    }
-    res.status(201).json({info: newInfo});
+  const {
+    name,
+    shortAddress,
+    address,
+    phoneNumber,
+    images,
+    intro,
+    fullIntro,
+    bestDrinksIntro,
+    hours,
+    hoursDetail,
+  } = req.body;
+  const newInfo = new Info({
+    name,
+    shortAddress,
+    address,
+    phoneNumber,
+    images,
+    intro,
+    fullIntro,
+    bestDrinksIntro,
+    hours,
+    hoursDetail,
+  });
+
+  try {
+    await newInfo.save();
+  } catch (err) {
+    const error = new HttpError('error when adding new info', 500);
+    return next(error);
+  }
+  res.status(201).json({ info: newInfo });
 };
-
-
-
 
 exports.getInfo = getInfo;
 exports.updateInfo = updateInfo;
--- server/controllers/menu-controllers.js
+++ server/controllers/menu-controllers.js
@@ -1,42 +1,142 @@
 // Controllers for menu
-
+const Menu = require('../models/product');
 // Import HttpError model
 const HttpError = require('../models/http-error');
+const menuControllers = require('../controllers/menu-controllers');
 
+const Product = require('../models/product');
 // Get All Menu
-const getAllMenu = (req, res, next) => {
-    console.log('get all menu');
-};
+const getAllMenu = async (req, res, next) => {
+  await Product.find()
+    .select('name price _id productImage content')
+    .exec()
+    .then((docs) => {
+      const response = {
+        count: docs.length,
+        products: docs.map((doc) => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            content: doc.content,
+            productImage: doc.productImage,
+            _id: doc._id,
+            request: {
+              type: 'GET',
+              url: 'http://localhost:5000/products/' + doc._id,
+            },
+          };
+        }),
+      };
 
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
 // Get All Drink
-const getAllDrinks = (req, res, next) => {
-    console.log('get all drink');
-};
+const getAllDrinks = (req, res, next) => {};
 
-// Get All Food 
+// Get All Food
 const getAllFood = (req, res, next) => {
-    console.log('get all food');
+  console.log('get all food');
 };
 
 // Add a new Item
-const addNewItem = (req, res, next) => {
-    console.log('add a new item');
+const addNewItem = async (req, res, next) => {
+  let fileUrl = req.file.path.replace(/\\/g, '/');
+
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price,
+    content: req.body.content,
+    productImage: fileUrl,
+  });
+  await product
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Created product successfully',
+        createdProduct: {
+          name: result.name,
+          price: result.price,
+          content: result.content,
+          _id: result._id,
+          request: {
+            type: 'GET',
+            url: 'http://localhost:5000/products/' + result._id,
+          },
+        },
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 };
 
 // Get item by Id
 const getItemById = (req, res, next) => {
-    const itemId = req.params.iid;
-    const foundItem = '';
-    console.log('get an item by id ' + itemId);
+  const itemId = req.params.iid;
+  console.log('get an item by id ' + itemId);
+  Product.findById(itemId)
+    .select('name price _id productImage')
+    .exec()
+    .then((doc) => {
+      console.log('From database', doc);
+      if (doc) {
+        res.status(200).json({
+          product: doc,
+          request: {
+            type: 'GET',
+            url: 'http://localhost:5000/products',
+          },
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: 'No valid entry found for provided ID' });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+};
 
-    if(!foundItem){
-        throw(new HttpError('can\'t found', 404));
-    }
-    res.json({foundItem});
-}
-
+const deleteById = async (req, res, next) => {
+  const itemId = req.params.iid;
+  console.log('delete an item by id ' + itemId);
+  await Product.remove({ _id: itemId })
+    .select('name price _id productImage')
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: 'Product deleted',
+        request: {
+          type: 'POST',
+          url: 'http://localhost:5000/products',
+          body: { name: 'String', price: 'Number' },
+        },
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
 exports.getAllMenu = getAllMenu;
 exports.getAllDrinks = getAllDrinks;
 exports.getAllFood = getAllFood;
 exports.addNewItem = addNewItem;
 exports.getItemById = getItemById;
+exports.deleteById = deleteById;
